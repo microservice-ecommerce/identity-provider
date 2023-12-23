ebfab2f287f1edf5ef98f64802f0025a619eedd1
ngocphuphamm <ngocphupham682001@gmail.com>
2023-12-24 00:03:29 +0700

    chore: update code

--- src/modules/auth/application/services/auth.service.ts
+++ src/modules/auth/application/services/auth.service.ts
@@ -2,13 +2,15 @@ import { H3Logger } from '@high3ar/common-api';
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserPayload, UserRequest, UserResponse } from '@user/core/dtos';
 import { IUserUseCase } from '@user/core/interfaces';
+import { USER_SERVICE } from '@user/core/token';
 import * as bcrypt from 'bcrypt';
+import { Request } from 'express';
 import Redis from 'ioredis';
-import { IdentityProviderConfig } from 'src/infrastructure/configuration/identity-provider.config';
-import { USER_SERVICE } from 'src/modules/user/core/token';
-import { LoginRequest, TokenPayload } from '../../core/dtos';
+import { IdentityProviderConfig } from '../../../../infrastructure/configuration/identity-provider.config';
+import { LoginRequest, TokenResponse } from '../../core/dtos';
 import { IAuthUseCase } from '../../core/interfaces';
 import { AuthHelper } from '../helpers/auth.helper';
+import { IdentityProviderConstant } from '@shared/core/constants';
 @Injectable()
 export class AuthService implements IAuthUseCase {
   constructor(
@@ -19,22 +21,36 @@ export class AuthService implements IAuthUseCase {
     private readonly _authHelper: AuthHelper,
   ) {}
 
-  public async login(request: LoginRequest): Promise<TokenPayload> {
-    const user: UserPayload = await this._userService.findOneByEmail(request.email);
+  public async login(req: Request, loginRequest: LoginRequest): Promise<TokenResponse> {
+    const user: UserPayload = await this._userService.findOneByEmail(loginRequest.email);
 
-    const isPasswordValid = await this._comparedPassword(request.password, user);
+    const isPasswordValid = await this._comparedPassword(loginRequest.password, user);
     if (!isPasswordValid) {
-      H3Logger.error(`Password not match with email: ${request.email}`);
+      H3Logger.error(`Password not match with email: ${loginRequest.email}`);
       throw new UnauthorizedException('Password not match');
     }
 
     const token = await this._authHelper.createToken(user);
-    return token;
+    this._authHelper.setCookies(req, token);
+
+    return new TokenResponse(token.accessToken, token.refreshToken, token.expiresInAccessToken);
   }
 
   public async register(request: UserRequest): Promise<UserResponse> {
     const user: UserPayload = await this._userService.save(request);
     return new UserResponse(user.infoUser, user.account);
+  }
+
+  public async refreshToken(req: Request): Promise<any> {
+    const refreshToken = req.cookies[IdentityProviderConstant.NAME_REFRESH_TOKEN];
+    if (!refreshToken) {
+      H3Logger.error('Refresh token not found');
+      throw new UnauthorizedException('Refresh token not found');
+    }
+
+    const tokenPayload = await this._authHelper.verifyRefreshToken(refreshToken);
+    const user = await this._userService.findOneByEmail(tokenPayload.email);
+    const token = await this._authHelper.createToken(user, tokenPayload.exp);
   }
 
   private async _comparedPassword(password: string, user: UserPayload): Promise<boolean> {
--- src/modules/auth/core/dtos/payloads/index.ts
+++ src/modules/auth/core/dtos/payloads/index.ts
@@ -1,2 +1,3 @@
 export * from './token.payload';
 export * from './encode-token-claim.payload';
+export * from './decode-token-claim.payload';
